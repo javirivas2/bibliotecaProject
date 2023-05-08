eed8b8fd3932e359a73128d907c80b6943bbc071
Manuel M <mackalvin@hotmail.com>
2023-05-08 17:17:42 +0200

    Clase Autor. version primera

--- src/main/java/com/capgemini/curso/model/Autor.java
+++ src/main/java/com/capgemini/curso/model/Autor.java
@@ -4,8 +4,66 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Autores")
+@Table(name = "autores")
 public class Autor {
+@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "nacionalidad")
+	private String nacionalidad;
+	@Column(name = "fecha_nacimiento")
+	private Date fechaNacimiento;
+	@OneToMany(mappedBy="autor",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+	private List<Libro>obras;
 
+	public Autor() {
+	}
+
+	public Autor(Long id, String nombre, String nacionalidad, Date fechaNacimiento) {
+		this.Id = id;
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [Id=" + Id + ", nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", fechaNacimiento="
+				+ fechaNacimiento + "]";
+	}
 	
 }
