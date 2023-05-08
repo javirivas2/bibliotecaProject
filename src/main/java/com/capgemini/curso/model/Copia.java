79fff3254f0968a2faade015c4ecf0a6f9e7b2c3
Manuel M <mackalvin@hotmail.com>
2023-05-08 17:46:24 +0200

    Clase Copia inicial

--- src/main/java/com/capgemini/curso/model/Copia.java
+++ src/main/java/com/capgemini/curso/model/Copia.java
@@ -7,11 +7,57 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "prestamos")
+@Table(name = "copias")
 public class Copia {
 //TODO: Sin acabar solo para probar el lector y prestamos
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long Id;
+	@Enumerated(EnumType.STRING)
+	private EstadoCopia estadoCopia;
+	@ManyToOne
+	@JoinColumn(name = "copia_id")
+	private Libro ejemplar;
+	@ManyToOne
+	@JoinColumn(name = "prestamo_id")
+	private Prestamo prestamo;
+
+	public Copia() {
+	}
+
+	public Copia(Long id, EstadoCopia estadoCopia, Libro ejemplar) {
+		this.Id = id;
+		this.estadoCopia = estadoCopia;
+		this.ejemplar = ejemplar;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public EstadoCopia getEstadoCopia() {
+		return estadoCopia;
+	}
+
+	public void setEstadoCopia(EstadoCopia estadoCopia) {
+		this.estadoCopia = estadoCopia;
+	}
+
+	public Libro getEjemplar() {
+		return ejemplar;
+	}
+
+	public void setEjemplar(Libro ejemplar) {
+		this.ejemplar = ejemplar;
+	}
+
+	@Override
+	public String toString() {
+		return "Copia [Id=" + Id + ", estadoCopia=" + estadoCopia + ", ejemplar=" + ejemplar + "]";
+	}
 }
