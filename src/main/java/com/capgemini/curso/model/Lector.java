c7c77b94cc8bb868eb03c31c2dfb78988328dac6
javirivas2 <80011597+javirivas2@users.noreply.github.com>
2023-05-16 17:32:45 +0200

    pagina libros con estilo reparado

--- src/main/java/com/capgemini/curso/model/Lector.java
+++ src/main/java/com/capgemini/curso/model/Lector.java
@@ -219,4 +219,30 @@ public class Lector {
 				+ ", username=" + username + ", password=" + password + ", roles=" + roles + ", multa=" + multa + "]";
 	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+	
+
 }
